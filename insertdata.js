cdff55cbe24a1a5aea559f5c2fb5d77f04360dbd
prpkr <kaihyvarinen@gmail.com>
2024-08-06 09:22:54 +0300

    Mongo DB

---
+++ insertdata.js
@@ -0,0 +1,610 @@
+var data = 
+[
+	{
+		"name": "Brennan",
+		"name1": "Tran",
+		"age": 34,
+		"city": "Urdaneta"
+	},
+	{
+		"name": "Violet",
+		"name1": "Duncan",
+		"age": 55,
+		"city": "Kettering"
+	},
+	{
+		"name": "Chloe",
+		"name1": "Lynch",
+		"age": 59,
+		"city": "Bedok"
+	},
+	{
+		"name": "Kaitlin",
+		"name1": "Daniels",
+		"age": 64,
+		"city": "Wals-Siezenheim"
+	},
+	{
+		"name": "Sonya",
+		"name1": "Davis",
+		"age": 59,
+		"city": "Sankt Ingbert"
+	},
+	{
+		"name": "Zane",
+		"name1": "Powell",
+		"age": 61,
+		"city": "Opprebais"
+	},
+	{
+		"name": "Reese",
+		"name1": "Cain",
+		"age": 30,
+		"city": "Cusco"
+	},
+	{
+		"name": "Leslie",
+		"name1": "Shaffer",
+		"age": 89,
+		"city": "La Dorada"
+	},
+	{
+		"name": "William",
+		"name1": "Puckett",
+		"age": 60,
+		"city": "Cork"
+	},
+	{
+		"name": "Baxter",
+		"name1": "English",
+		"age": 68,
+		"city": "Manavgat"
+	},
+	{
+		"name": "Donna",
+		"name1": "Torres",
+		"age": 72,
+		"city": "Bishan"
+	},
+	{
+		"name": "Vance",
+		"name1": "Sherman",
+		"age": 59,
+		"city": "Chépica"
+	},
+	{
+		"name": "Oprah",
+		"name1": "Rodgers",
+		"age": 93,
+		"city": "Chortkiv"
+	},
+	{
+		"name": "Hammett",
+		"name1": "Wall",
+		"age": 95,
+		"city": "Nelson"
+	},
+	{
+		"name": "Allistair",
+		"name1": "Munoz",
+		"age": 67,
+		"city": "Jeju"
+	},
+	{
+		"name": "Brianna",
+		"name1": "Knapp",
+		"age": 25,
+		"city": "Mukachevo"
+	},
+	{
+		"name": "McKenzie",
+		"name1": "Bonner",
+		"age": 46,
+		"city": "Nogales"
+	},
+	{
+		"name": "Martin",
+		"name1": "Gillespie",
+		"age": 41,
+		"city": "Alcalá de Henares"
+	},
+	{
+		"name": "Irene",
+		"name1": "Huffman",
+		"age": 67,
+		"city": "Westport"
+	},
+	{
+		"name": "Victor",
+		"name1": "Sykes",
+		"age": 40,
+		"city": "Kasur"
+	},
+	{
+		"name": "Cara",
+		"name1": "Sanford",
+		"age": 64,
+		"city": "Warszawa"
+	},
+	{
+		"name": "Yolanda",
+		"name1": "Dudley",
+		"age": 64,
+		"city": "Nelspruit"
+	},
+	{
+		"name": "Fitzgerald",
+		"name1": "Savage",
+		"age": 19,
+		"city": "Minatitlán"
+	},
+	{
+		"name": "Francis",
+		"name1": "Livingston",
+		"age": 63,
+		"city": "Emalahleni"
+	},
+	{
+		"name": "Colby",
+		"name1": "Kidd",
+		"age": 45,
+		"city": "Linköping"
+	},
+	{
+		"name": "Damian",
+		"name1": "Goff",
+		"age": 50,
+		"city": "Henan"
+	},
+	{
+		"name": "Ciara",
+		"name1": "Coffey",
+		"age": 79,
+		"city": "Saratov"
+	},
+	{
+		"name": "Lillith",
+		"name1": "Brock",
+		"age": 22,
+		"city": "Kano"
+	},
+	{
+		"name": "Ryder",
+		"name1": "Sampson",
+		"age": 77,
+		"city": "Tournefeuille"
+	},
+	{
+		"name": "Darrel",
+		"name1": "Hart",
+		"age": 98,
+		"city": "Pietermaritzburg"
+	},
+	{
+		"name": "Aileen",
+		"name1": "Mckinney",
+		"age": 78,
+		"city": "Kherson"
+	},
+	{
+		"name": "Wynter",
+		"name1": "Johnson",
+		"age": 25,
+		"city": "Florø"
+	},
+	{
+		"name": "Clio",
+		"name1": "Conner",
+		"age": 71,
+		"city": "Whyalla"
+	},
+	{
+		"name": "Rogan",
+		"name1": "Santos",
+		"age": 58,
+		"city": "Ebenthal in Kärnten"
+	},
+	{
+		"name": "Hadassah",
+		"name1": "Wells",
+		"age": 65,
+		"city": "Bowling Green"
+	},
+	{
+		"name": "Macaulay",
+		"name1": "Mcguire",
+		"age": 32,
+		"city": "Barkhan"
+	},
+	{
+		"name": "Thomas",
+		"name1": "Holt",
+		"age": 78,
+		"city": "Abbottabad"
+	},
+	{
+		"name": "Hilary",
+		"name1": "Velazquez",
+		"age": 96,
+		"city": "Paiguano"
+	},
+	{
+		"name": "Ashton",
+		"name1": "Serrano",
+		"age": 28,
+		"city": "Jackson"
+	},
+	{
+		"name": "Len",
+		"name1": "Bradley",
+		"age": 75,
+		"city": "Bafra"
+	},
+	{
+		"name": "Alexander",
+		"name1": "Bean",
+		"age": 79,
+		"city": "Việt Trì"
+	},
+	{
+		"name": "Aretha",
+		"name1": "Burch",
+		"age": 27,
+		"city": "Lillesand"
+	},
+	{
+		"name": "Owen",
+		"name1": "Marsh",
+		"age": 26,
+		"city": "Vị Thanh"
+	},
+	{
+		"name": "Brielle",
+		"name1": "Sykes",
+		"age": 53,
+		"city": "Villingen-Schwenningen"
+	},
+	{
+		"name": "Raven",
+		"name1": "Craft",
+		"age": 97,
+		"city": "São José dos Pinhais"
+	},
+	{
+		"name": "Kyra",
+		"name1": "Puckett",
+		"age": 66,
+		"city": "Mohmand Agency"
+	},
+	{
+		"name": "Iliana",
+		"name1": "Pate",
+		"age": 32,
+		"city": "Farsund"
+	},
+	{
+		"name": "Rhiannon",
+		"name1": "Herrera",
+		"age": 84,
+		"city": "Tejar"
+	},
+	{
+		"name": "Brady",
+		"name1": "Barnett",
+		"age": 82,
+		"city": "Lowell"
+	},
+	{
+		"name": "Cecilia",
+		"name1": "Avery",
+		"age": 95,
+		"city": "Kidapawan"
+	},
+	{
+		"name": "Skyler",
+		"name1": "Boyer",
+		"age": 66,
+		"city": "Poltava"
+	},
+	{
+		"name": "Nicholas",
+		"name1": "Vang",
+		"age": 69,
+		"city": "Potchefstroom"
+	},
+	{
+		"name": "Macy",
+		"name1": "Harrell",
+		"age": 26,
+		"city": "Tibet"
+	},
+	{
+		"name": "Cathleen",
+		"name1": "Britt",
+		"age": 59,
+		"city": "Candon"
+	},
+	{
+		"name": "Cecilia",
+		"name1": "Strickland",
+		"age": 32,
+		"city": "Frederikstad"
+	},
+	{
+		"name": "Aidan",
+		"name1": "England",
+		"age": 24,
+		"city": "Chepén"
+	},
+	{
+		"name": "Leroy",
+		"name1": "Kane",
+		"age": 97,
+		"city": "Inner Mongolia"
+	},
+	{
+		"name": "Oliver",
+		"name1": "Waters",
+		"age": 56,
+		"city": "Lublin"
+	},
+	{
+		"name": "Lisandra",
+		"name1": "Kaufman",
+		"age": 53,
+		"city": "Tire"
+	},
+	{
+		"name": "Leandra",
+		"name1": "Vincent",
+		"age": 18,
+		"city": "Alto Hospicio"
+	},
+	{
+		"name": "Reuben",
+		"name1": "England",
+		"age": 39,
+		"city": "Illapel"
+	},
+	{
+		"name": "Garth",
+		"name1": "Cooke",
+		"age": 19,
+		"city": "Anklam"
+	},
+	{
+		"name": "Hasad",
+		"name1": "Prince",
+		"age": 58,
+		"city": "Ife"
+	},
+	{
+		"name": "August",
+		"name1": "O'Neill",
+		"age": 82,
+		"city": "Belford Roxo"
+	},
+	{
+		"name": "Minerva",
+		"name1": "Barrera",
+		"age": 55,
+		"city": "Huaraz"
+	},
+	{
+		"name": "Eve",
+		"name1": "Macias",
+		"age": 54,
+		"city": "Gianico"
+	},
+	{
+		"name": "Buckminster",
+		"name1": "Gilmore",
+		"age": 38,
+		"city": "Gbongan"
+	},
+	{
+		"name": "Zeph",
+		"name1": "Wilkerson",
+		"age": 20,
+		"city": "Toowoomba"
+	},
+	{
+		"name": "Mariko",
+		"name1": "Daugherty",
+		"age": 92,
+		"city": "Vilcún"
+	},
+	{
+		"name": "Althea",
+		"name1": "Estes",
+		"age": 39,
+		"city": "Mala"
+	},
+	{
+		"name": "Priscilla",
+		"name1": "Mcknight",
+		"age": 54,
+		"city": "Vauda Canavese"
+	},
+	{
+		"name": "Marshall",
+		"name1": "Paul",
+		"age": 71,
+		"city": "Guadalajara"
+	},
+	{
+		"name": "Darrel",
+		"name1": "Wagner",
+		"age": 83,
+		"city": "Kufstein"
+	},
+	{
+		"name": "Kadeem",
+		"name1": "Schwartz",
+		"age": 90,
+		"city": "Serang"
+	},
+	{
+		"name": "Justin",
+		"name1": "Long",
+		"age": 37,
+		"city": "Cabo de Hornos"
+	},
+	{
+		"name": "Benjamin",
+		"name1": "Singleton",
+		"age": 68,
+		"city": "Ereğli"
+	},
+	{
+		"name": "Oliver",
+		"name1": "Medina",
+		"age": 18,
+		"city": "Hope"
+	},
+	{
+		"name": "Dolan",
+		"name1": "Glenn",
+		"age": 76,
+		"city": "Paarl"
+	},
+	{
+		"name": "Benjamin",
+		"name1": "Camacho",
+		"age": 54,
+		"city": "Chulucanas"
+	},
+	{
+		"name": "Mechelle",
+		"name1": "Carter",
+		"age": 40,
+		"city": "Wolfsberg"
+	},
+	{
+		"name": "Kuame",
+		"name1": "Murray",
+		"age": 74,
+		"city": "Mount Gambier"
+	},
+	{
+		"name": "Kaye",
+		"name1": "Mueller",
+		"age": 96,
+		"city": "Villingen-Schwenningen"
+	},
+	{
+		"name": "Kylan",
+		"name1": "Lawrence",
+		"age": 71,
+		"city": "Palu"
+	},
+	{
+		"name": "Aurelia",
+		"name1": "Mccullough",
+		"age": 54,
+		"city": "Arica"
+	},
+	{
+		"name": "Judah",
+		"name1": "Miller",
+		"age": 96,
+		"city": "Chapadinha"
+	},
+	{
+		"name": "Colt",
+		"name1": "Christensen",
+		"age": 42,
+		"city": "Choa Chu Kang"
+	},
+	{
+		"name": "Amal",
+		"name1": "Whitley",
+		"age": 31,
+		"city": "Ila"
+	},
+	{
+		"name": "Marshall",
+		"name1": "Alford",
+		"age": 71,
+		"city": "Wedel"
+	},
+	{
+		"name": "Ciaran",
+		"name1": "Wiley",
+		"age": 62,
+		"city": "Birori"
+	},
+	{
+		"name": "Marny",
+		"name1": "Delaney",
+		"age": 55,
+		"city": "Stavanger"
+	},
+	{
+		"name": "Melodie",
+		"name1": "Salas",
+		"age": 49,
+		"city": "Oslo"
+	},
+	{
+		"name": "Veronica",
+		"name1": "Mccormick",
+		"age": 40,
+		"city": "Brummunddal"
+	},
+	{
+		"name": "Laurel",
+		"name1": "Savage",
+		"age": 18,
+		"city": "Onitsha"
+	},
+	{
+		"name": "Joseph",
+		"name1": "Black",
+		"age": 96,
+		"city": "Panabo"
+	},
+	{
+		"name": "Unity",
+		"name1": "Pollard",
+		"age": 70,
+		"city": "San Vicente del Caguán"
+	},
+	{
+		"name": "Melodie",
+		"name1": "Evans",
+		"age": 21,
+		"city": "Pamplona"
+	},
+	{
+		"name": "Selma",
+		"name1": "O'connor",
+		"age": 86,
+		"city": "Salisbury"
+	},
+	{
+		"name": "Joelle",
+		"name1": "Kelley",
+		"age": 63,
+		"city": "Shrewsbury"
+	},
+	{
+		"name": "Addison",
+		"name1": "Hicks",
+		"age": 78,
+		"city": "Shillong"
+	},
+	{
+		"name": "Jorden",
+		"name1": "Stone",
+		"age": 27,
+		"city": "Dir"
+	}
+]
+
+
+//Mongon omaa javasciptimäistä kieltä
+var conn = new Mongo("mongodb+srv://fullstack:password@cluster0.wcy6njv.mongodb.net/")
+var db = conn.getDB("contactdata")
+
+db.contacts.insertMany(data)
